da7d51c3c516e3ec4ee0269a058391430eb3789c
Arpit Anand <invoker.arpit@gmail.com>
2025-01-24 18:33:37 +0530

    frontend initialized

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+function App() {
+
+  return (
+    <>
+      <div>
+        <p className='text-3xl font-bold underline'>Hello</p>
+        </div>
+    </>
+  )
+}
+
+export default App
