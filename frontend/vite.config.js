3043846c82be3b2a709461142f7852104edd2784
Arpit Anand <invoker.arpit@gmail.com>
2025-03-26 22:08:09 +0530

    frontend deployed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   
-  plugins: [react(),tailwindcss()]
+  plugins: [react(),tailwindcss()],
+  build: {
+    outDir: '../public',  // This will place the build output in the public folder of Firebase Hosting
+  },
 })
+
